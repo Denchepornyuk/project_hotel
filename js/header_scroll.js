50422a21f3cba0c0499da6d840ca591898d636ab
Denchepornyuk <cheporniuk98@gmail.com>
2019-05-10 15:36:59 +0300

    sub menu

--- js/header_scroll.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1){  
-        $('header').addClass("scroll");
-        $('ul.submenu').addClass('submenu__scroll');
-    }  else{
-        $('header').removeClass("scroll");
-        $('ul.submenu').removeClass('submenu__scroll');
-    }
-});
